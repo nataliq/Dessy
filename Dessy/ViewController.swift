e2a8a8406eed8b0137028b6210d94ea27b39f8ec
Nataliya Patsovska <nataliya.patsovska@izettle.com>
2018-04-22 11:39:00 +0200

    Create initial empty project

---
+++ Dessy/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Dessy
+//
+//  Created by Nataliya Patsovska on 2018-04-22.
+//  Copyright © 2018 Nataliya Patsovska. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
